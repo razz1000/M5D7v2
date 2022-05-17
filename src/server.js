2fd5d532b1d63db11231cd944cb442e1936b44d4
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-05-17 15:11:43 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,33 +1,33 @@
-import express from 'express'
-import listEndpoints from 'express-list-endpoints'
-import productsRouter from './apis/products/index.js'
-import cors from 'cors'
-import { join } from 'path'
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import productsRouter from "./apis/products/index.js";
+import cors from "cors";
+import { join } from "path";
 
-const publicFolderPath = join(process.cwd(), './public')
+const publicFolderPath = join(process.cwd(), "./public");
 
-const server = express()
+const server = express();
 
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5001;
 
-const whitelist = []
+const whitelist = [process.env.BE_DEV_URL, process.env.BE_PROD_URL];
 const corsOptions = {
-  origin: 'http://frontendapp.com'
-}
+  origin: "http://frontendapp.com",
+};
 
-server.use(cors())
+server.use(cors());
 
-server.use(express.static(publicFolderPath))
-server.use(express.json())
+server.use(express.static(publicFolderPath));
+server.use(express.json());
 
-server.use('/products', productsRouter)
+server.use("/products", productsRouter);
 
-server.use(express.static(publicFolderPath))
+server.use(express.static(publicFolderPath));
 
 server.listen(port, () => {
-  console.table(listEndpoints(server))
-  console.log(`Server is listening on port ${port}`)
-})
-server.on('error', (error) => {
-  console.log('new error', error)
-})
+  console.table(listEndpoints(server));
+  console.log(`Server is listening on port ${port}`);
+});
+server.on("error", (error) => {
+  console.log("new error", error);
+});
